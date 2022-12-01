1bfce19213a6b2c199521ba766270e0080e82738
Avi Schwab <froboy@gmail.com>
2022-12-01 15:00:17 -0600

    Updates

---
+++ precache-manifest.f68ebec9def229bde1b90b5f54f0a991.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e48f24ef834d60d61ede22f417d5d73d",
+    "url": "/y-ipsum/index.html"
+  },
+  {
+    "revision": "b3fff41698374439ca0e",
+    "url": "/y-ipsum/static/css/main.6ea095f4.chunk.css"
+  },
+  {
+    "revision": "c8cf8049874653e89568",
+    "url": "/y-ipsum/static/js/2.befeda33.chunk.js"
+  },
+  {
+    "revision": "5b571a43117d7de158db9e221eaa0225",
+    "url": "/y-ipsum/static/js/2.befeda33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3fff41698374439ca0e",
+    "url": "/y-ipsum/static/js/main.76a6094e.chunk.js"
+  },
+  {
+    "revision": "3e3bba7861a80e8e7fff",
+    "url": "/y-ipsum/static/js/runtime-main.e176deb9.js"
+  }
+]);
